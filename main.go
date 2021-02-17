3de085a470fe768976a34f41109dce01544485fd
Alex Orfanos <alexandros@mailerlite.com>
2021-02-17 20:02:54 +0200

    add wip configuration

--- main.go
+++ main.go
@@ -2,36 +2,50 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gopkg.in/yaml.v2"
 )
 
 var (
-	portFlagPtr := flag.Int("port", 11011, "The port to expose metrics to")
+	portNum    = flag.Int("port", 11011, "The port to expose metrics to")
+	configFile = flag.String("config.file", "wordpress-exporter.yml", "Configure which WordPress sites to monitor")
 )
+
+func init() {
+	flag.Parse()
+	prometheus.MustRegister(newWordpressCollector())
+}
 
 func main() {
 	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(string(*portFlagPtr), nil))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*portNum), nil))
 }
 
 type Wordpress struct {
-	posts, categories, tags, pages, comments, media, users, admin_users, themes int 
+	posts, categories, tags, pages, comments, media, users, adminUsers, themes int
 }
 
 type WordpressCollector struct {
-	posts *prometheus.Desc
+	posts      *prometheus.Desc
 	categories *prometheus.Desc
-	tags *prometheus.Desc
-	pages *prometheus.Desc
-	comments *prometheus.Desc
-	media *prometheus.Desc
-	users *prometheus.Desc
-	admin_users *prometheus.Desc
-	themes *prometheus.Desc
+	tags       *prometheus.Desc
+	pages      *prometheus.Desc
+	comments   *prometheus.Desc
+	media      *prometheus.Desc
+	users      *prometheus.Desc
+	adminUsers *prometheus.Desc
+	themes     *prometheus.Desc
+}
+
+type ConfigFile struct {
+	MonitoredWordpress []string `yaml:"wordpress-exporter"`
 }
 
 func (c *WordpressCollector) Describe(ch chan<- *prometheus.Desc) {
@@ -42,27 +56,59 @@ func (c *WordpressCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.comments
 	ch <- c.media
 	ch <- c.users
-	ch <- c.admin_users
+	ch <- c.adminUsers
 	ch <- c.themes
 }
 
 func newWordpressCollector() *WordpressCollector {
 	return &WordpressCollector{
-		posts: prometheus.NewDesc("wordpress_post_count", "WordPress posts count", nil, nil),
+		posts:      prometheus.NewDesc("wordpress_post_count", "WordPress posts count", nil, nil),
 		categories: prometheus.NewDesc("wordpress_category_count", "WordPress category count", nil, nil),
-		tags: prometheus.NewDesc("wordpress_tag_count", "WordPress tags count", nil, nil),
-		pages: prometheus.NewDesc("wordpress_page_count", "WordPress pages count", nil, nil),
-		comments: prometheus.NewDesc("wordpress_comment_count", "WordPress comments count", nil, nil),
-		media: prometheus.NewDesc("wordpress_media_count", "WordPress media files count", nil, nil),
-		users: prometheus.NewDesc("wordpress_user_count", "WordPress users count", nil, nil),
-		admin_users: prometheus.NewDesc("wordpress_admin_user_count", "WordPress administrator-level user count", nil, nil),
-		themes: prometheus.NewDesc("wordpress_theme_count", "WordPress theme count", nil, nil),
+		tags:       prometheus.NewDesc("wordpress_tag_count", "WordPress tags count", nil, nil),
+		pages:      prometheus.NewDesc("wordpress_page_count", "WordPress pages count", nil, nil),
+		comments:   prometheus.NewDesc("wordpress_comment_count", "WordPress comments count", nil, nil),
+		media:      prometheus.NewDesc("wordpress_media_count", "WordPress media files count", nil, nil),
+		users:      prometheus.NewDesc("wordpress_user_count", "WordPress users count", nil, nil),
+		adminUsers: prometheus.NewDesc("wordpress_admin_user_count", "WordPress administrator-level user count", nil, nil),
+		themes:     prometheus.NewDesc("wordpress_theme_count", "WordPress theme count", nil, nil),
 	}
 }
+
+func (c *ConfigFile) ParseConf() *ConfigFile {
+	yamlFile, err := ioutil.ReadFile(*configFile)
+	errCheck(err)
+	err = yaml.Unmarshal(yamlFile, c)
+	errCheck(err)
+	return c
+}
+
+func FetchJSONFromEndpoint(APIBase, APIEndpoint string) []byte {
+	fetchURL := fmt.Sprintf("%s%s", APIBase, APIEndpoint)
+	response, err := http.Get(fetchURL)
+	errCheck(err)
+	data, _ := ioutil.ReadAll(response.Body)
+	return data
+}
+
+func CountJSONItems(JSONResponse []byte) {
+
+}
+
+func (c *WordpressCollector) Collect(ch chan<- prometheus.Metric) {
+	var _wordpress = new(Wordpress)
+	_wordpress.categories = 2
+	ch <- prometheus.MustNewConstMetric(c.categories, prometheus.GaugeValue, float64(_wordpress.categories))
+}
+
+// func CountItemsFromEndpoint(endpoint string) {
+// 	sites :=
+// 	for _, service := range sites.ParseConf().MonitoredWordpress {
+// 		fmt.Println("", service)
+// 	}
+// }
 
 func errCheck(e error) {
 	if e != nil {
 		log.Println(e)
 	}
 }
-
